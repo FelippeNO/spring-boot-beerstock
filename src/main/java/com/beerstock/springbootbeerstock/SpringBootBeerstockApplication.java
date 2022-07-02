87e479dfdb31a1e38275d4fe92396453376b765e
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-07-02 11:06:48 -0300

    Initial commit

---
+++ src/main/java/com/beerstock/springbootbeerstock/SpringBootBeerstockApplication.java
@@ -0,0 +1,13 @@
+package com.beerstock.springbootbeerstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBeerstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBeerstockApplication.class, args);
+	}
+
+}
