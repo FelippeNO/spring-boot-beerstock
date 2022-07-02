ec99f46e455ea0b9bda2cd40d28f2407478df722
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-07-02 15:59:44 -0300

    Update SwaggerConfig.java

--- src/main/java/com/beerstock/springbootbeerstock/config/SwaggerConfig.java
+++ src/main/java/com/beerstock/springbootbeerstock/config/SwaggerConfig.java
@@ -16,12 +16,12 @@ import static springfox.documentation.builders.RequestHandlerSelectors.*;
 @EnableSwagger2
 public class SwaggerConfig {
 
-    private static final String BASE_PACKAGE = "one.digitalinnovation.beerstock.controller";
+    private static final String BASE_PACKAGE = "com.beerstock.springbootbeerstock";
     private static final String API_TITLE = "Beer Stock API";
     private static final String API_DESCRIPTION = "REST API for beer stock management";
-    private static final String CONTACT_NAME = "Rodrigo Peleias";
-    private static final String CONTACT_GITHUB = "https://gtihub.com/rpeleias";
-    private static final String CONTACT_EMAIL = "rodrigo.peleis@gmail.com";
+    private static final String CONTACT_NAME = "Felippe Negrão";
+    private static final String CONTACT_GITHUB = "https://github.com/FelippeNO";
+    private static final String CONTACT_EMAIL = "felippeno@gmail.com";
 
     @Bean
     public Docket api() {
