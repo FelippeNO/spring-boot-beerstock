417b96c3a4abf496ad576084f0eaab7cdd064af7
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-07-03 12:32:38 -0300

    Added controller test

--- src/main/java/com/beerstock/springbootbeerstock/controller/BeerController.java
+++ src/main/java/com/beerstock/springbootbeerstock/controller/BeerController.java
@@ -10,36 +10,38 @@ import com.beerstock.springbootbeerstock.exception.BeerNotFoundException;
 import com.beerstock.springbootbeerstock.service.BeerService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/beers")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
-
 public class BeerController implements BeerControllerDocs {
 
     private final BeerService beerService;
 
-    @Override
-    public BeerDTO createBeer(BeerDTO beerDTO) throws BeerAlreadyRegisteredException {
-        return null;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BeerDTO createBeer(@RequestBody @Valid BeerDTO beerDTO) throws BeerAlreadyRegisteredException {
+        return beerService.createBeer(beerDTO);
     }
 
-    @Override
-    public BeerDTO findByName(String name) throws BeerNotFoundException {
-        return null;
+    @GetMapping("/{name}")
+    public BeerDTO findByName(@PathVariable String name) throws BeerNotFoundException {
+        return beerService.findByName(name);
     }
 
-    @Override
+    @GetMapping
     public List<BeerDTO> listBeers() {
-        return null;
+        return beerService.listAll();
     }
 
-    @Override
-    public void deleteById(Long id) throws BeerNotFoundException {
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws BeerNotFoundException {
 
     }
 }
---
+++ src/test/java/com/beerstock/springbootbeerstock/controller/BeerControllerTest.java
@@ -0,0 +1,67 @@
+package com.beerstock.springbootbeerstock.controller;
+
+import com.beerstock.springbootbeerstock.builder.BeerDTOBuilder;
+import com.beerstock.springbootbeerstock.dto.BeerDTO;
+import com.beerstock.springbootbeerstock.service.BeerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import static com.beerstock.springbootbeerstock.utils.JsonConvertionUtils.asJsonString;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+public class BeerControllerTest {
+
+    private static final String BEER_API_URL_PATH = "/api/v1/beers";
+    private static final long VALID_BEER_ID = 1L;
+    private static final long INVALID_BEER_ID = 2l;
+    private static final String BEER_API_SUBPATH_INCREMENT_URL = "/increment";
+    private static final String BEER_API_SUBPATH_DECREMENT_URL = "/decrement";
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private BeerService beerService;
+
+    @InjectMocks
+    private BeerController beerController;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(beerController)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .setViewResolvers((s, locale) -> new MappingJackson2JsonView())
+                .build();
+    }
+
+    @Test
+    void whenPOSTIsCalledThenABeerIsCreated() throws Exception {
+        // given
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+
+        // when
+        when(beerService.createBeer(beerDTO)).thenReturn(beerDTO);
+
+        // then
+        mockMvc.perform(post(BEER_API_URL_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(beerDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name", is(beerDTO.getName())))
+                .andExpect(jsonPath("$.brand", is(beerDTO.getBrand())))
+                .andExpect(jsonPath("$.type", is(beerDTO.getType().toString())));
+    }
+}
